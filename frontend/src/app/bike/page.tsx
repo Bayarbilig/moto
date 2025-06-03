2eb855a784bbce0eacf46342e32280a1f9c342a9
Bayarbilig <u.bayrbilig@gmail.com>
2025-06-03 16:31:04 +0800

    speed to power

--- frontend/src/app/bike/page.tsx
+++ frontend/src/app/bike/page.tsx
@@ -53,7 +53,9 @@ const Page = () => {
             <p className="text-sm text-gray-400 mb-2">{bike.bikeModel}</p>
             <div className="flex justify-between text-sm text-gray-300">
               <span>Моторын багтаамж: {bike.cc}</span>
-              <span>Хүч: {bike.power}</span>
+              <span>Хүч: {bike.power}
+                
+              </span>
             </div>
           </div>
         ))}
