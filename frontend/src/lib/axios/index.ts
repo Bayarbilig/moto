c90ccb632bc23362da54a6dfce5ec5bd73e9ec55
Bayarbilig <u.bayrbilig@gmail.com>
2025-09-10 11:59:38 +0800

    fe axios baseURL

--- frontend/src/lib/axios/index.ts
+++ frontend/src/lib/axios/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://moto-backend-rv02.onrender.com",
+  baseURL: "https://moto-backend-rv02.onrender.com", 
   // baseURL: "http://192.168.1.99:5000",
   // baseURL: "http://localhost:5000",
   headers: {
